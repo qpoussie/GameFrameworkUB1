c43124658da848c4fadb8f620e786ceacf07698f
QuentinP <quentin.poussier@gmail.com>
2014-01-30 02:18:30 +0100

    adding some sounds ! 2

--- Pacman/src/linkstr/Main.java
+++ Pacman/src/linkstr/Main.java
@@ -2,17 +2,29 @@ package linkstr;
 
 import gameframework.game.GameDefaultImpl;
 import gameframework.game.GameLevel;
+import gameframeworkExtension.Sound;
 
 import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
+
+		/* for test on JAR
+		Sound s = new Sound("C:/Users/Quentin/Documents/GitHub/GameFrameworkUB1/Pacman/background.wav");
+		s.loop();
+		*/
+		Sound s = new Sound("sounds/background.wav");
+		s.loop();
+		
 		GameDefaultImpl g = new GameDefaultImpl();
 		ArrayList<GameLevel> levels = new ArrayList<GameLevel>();
 
 		levels.add(new GameLevelOne(g)); // only one level is available at this time
 		
 		g.setLevels(levels);
+
+		Sound o = new Sound("sounds/opening.wav");
+		o.play();
 		g.start();
 	}
 }
